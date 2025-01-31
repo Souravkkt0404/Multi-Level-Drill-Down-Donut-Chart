dd7fcbda416f55034b0698bb66e6946e9b78e9c1
sourav <schatterjee.0404@gmail.com>
2025-01-31 18:24:26 +0530

    add components folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import CreateDoughnutData from './CreateDoughnutData';
+import CreateDoughnutData from './Components/CreateDoughnutData';
 
 function App() {
   return (
